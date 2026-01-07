d8f891dc08f6e473f58f15fed828a94551066266
SunnyDev7 <sunnydev4589@gmail.com>
2026-01-07 16:00:28 +0100

    session based authentication done

---
+++ routes/user.routes.js
@@ -0,0 +1,82 @@
+import express from "express";
+import { eq } from "drizzle-orm";
+import { randomBytes, createHmac } from "node:crypto";
+
+import db from "../db/index.js";
+import { usersTable, userSessions } from "../db/schema.js";
+
+const router = express.Router();
+
+router.get("/"); //show to logged in user
+
+router.post("/signup", async function (req, res) {
+  const { name, email, password } = req.body;
+
+  const [existingUser] = await db
+    .select({
+      email: usersTable.email,
+    })
+    .from(usersTable)
+    .where((table) => eq(table.email, email));
+
+  if (existingUser) {
+    return res.status(400).json({ error: "User with email already exists" });
+  }
+
+  const salt = randomBytes(256).toString("hex");
+  const hashedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  const [user] = await db
+    .insert(usersTable)
+    .values({
+      name,
+      email,
+      password: hashedPassword,
+      salt,
+    })
+    .returning({ id: usersTable.id });
+
+  return res
+    .status(201)
+    .json({ status: "Success!", data: { userId: user.id } });
+});
+
+router.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+
+  const [existingUser] = await db
+    .select({
+      id: usersTable.id,
+      email: usersTable.email,
+      salt: usersTable.salt,
+      password: usersTable.password,
+    })
+    .from(usersTable)
+    .where((table) => eq(table.email, email));
+
+  if (!existingUser) {
+    return res.status(404).json({ error: "User with email does not exists" });
+  }
+
+  const salt = existingUser.salt;
+  const existingHash = existingUser.password;
+
+  const newHash = createHmac("sha256", salt).update(password).digest("hex");
+
+  if (newHash !== existingHash) {
+    return res.status(400).json({ erro: "Incorrect Password" });
+  }
+
+  //generate a session for the user and return success
+  const [session] = await db
+    .insert(userSessions)
+    .values({ userId: existingUser.id })
+    .returning({
+      id: userSessions.id,
+    });
+  return res.json({ status: "success!", sessionId: session.id });
+});
+
+export default router;
